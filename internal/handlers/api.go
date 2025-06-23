a9cda0d19ef271d90ed7d86f42cebd27a1785f13
sagir <sagir.jusupv2014@gmail.com>
2025-06-23 16:55:17 +0500

    add git ignore

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -2,18 +2,31 @@ package handlers_
 
 import (
 	"awesomeProject1/internal/db"
+	"awesomeProject1/internal/models"
 	"encoding/json"
 	"net/http"
 )
 
 func InitHandlers() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
 			students := db_.SelectStudent()
 			json.NewEncoder(w).Encode(students)
 		} else if r.Method == "POST" {
-			//В будущем что то да и сделаю
-
+			student := models.Student{}
+			err := json.NewDecoder(r.Body).Decode(&student)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusBadRequest)
+				return
+			}
+			db_.InsertStudent(student)
+			if err := json.NewEncoder(w).Encode(student); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		} else {
+			http.Error(w, "Метод не найден", http.StatusMethodNotAllowed)
 		}
 	})
+
 }
