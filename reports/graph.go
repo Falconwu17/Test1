adfbe6062819a9b847badab9e8123e3741c03c12
sagir <sagir.jusupv2014@gmail.com>
2025-07-10 16:46:39 +0500

    Change graph

--- reports/graph.go
+++ reports/graph.go
@@ -3,35 +3,34 @@ package reports
 import (
 	db_ "awesomeProject1/internal/db"
 	"awesomeProject1/internal/models"
+	"bytes"
 	"encoding/json"
+	"errors"
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/plotutil"
 	"gonum.org/v1/plot/vg"
 	"log"
 	"net/http"
-	"os"
 )
 
 func GraphHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		const path = "graph.png"
-		if err := Graph(); err != nil {
+		buf := new(bytes.Buffer)
+		if err := Graph(buf); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		w.Header().Set("Content-Type", "image/png")
-		http.ServeFile(w, r, path)
+		w.Write(buf.Bytes())
 	}
 }
 
-func Graph() error {
-	const path = "graph.png"
-	_ = os.Remove(path)
+func Graph(buf *bytes.Buffer) error {
 	p := plot.New()
 	p.Title.Text = "Данные Record и Entry по времени"
 	p.X.Label.Text = "Время"
-	p.Y.Label.Text = "Значение"
+	p.Y.Label.Text = "Значение Value JSON"
 	p.X.Tick.Marker = plot.TimeTicks{Format: "2006-01-02 15:04:05"}
 
 	recordsTable, err := db_.SelectRecord(100, 0)
@@ -46,14 +45,22 @@ func Graph() error {
 	}
 	pointsRecord := pointsForRecords(recordsTable)
 	pointsEntry := pointsForEntry(entriesTable)
-
-	if err := plotutil.AddLinePoints(p,
-		"Record", pointsRecord,
-		"Entry", pointsEntry,
-	); err != nil {
+	p.Add(plotter.NewGrid())
+	if err := plotutil.AddLinePoints(p, "Record", pointsRecord,
+		"Entry", pointsEntry); err != nil {
 		return err
 	}
-	return p.Save(8*vg.Inch, 5*vg.Inch, path)
+	writerTo, err := p.WriterTo(8*vg.Inch, 5*vg.Inch, "png")
+	if err != nil {
+		return err
+	}
+	if len(pointsRecord) == 0 && len(pointsEntry) == 0 {
+		return errors.New("нет данных для построения графика")
+	}
+
+	_, err = writerTo.WriteTo(buf)
+	return err
+
 }
 
 func pointsForRecords(recordsTable []models.Record) plotter.XYs {
@@ -72,6 +79,10 @@ func pointsForEntry(entriesTable []models.Entry) plotter.XYs {
 	for _, entry := range entriesTable {
 		var payload struct {
 			Value float64 `json:"value"`
+		}
+		if !json.Valid(entry.Data) {
+			log.Printf("⚠ Invalid JSON in entry ID=%d: %s", entry.Id, string(entry.Data))
+			continue
 		}
 		if err := json.Unmarshal(entry.Data, &payload); err != nil {
 			log.Println("entry.Data parse error:", err)
